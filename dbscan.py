16e6fcf96410131f05cb175412ab0edb94176f53
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-22 16:32:57 +0430

    Update : delete additional print

--- dbscan.py
+++ dbscan.py
@@ -15,24 +15,25 @@ dists = []
 
 
 def _eps_neighborhood(p, q, eps):
-    print("Run _eps_neighborhood")
+    # print("Run _eps_neighborhood")
     return dists[p][q] < eps
 
 
 def _region_query(m, point_id, eps):
-    print("Run _region_query")
+    # print("Run _region_query")
     n_points = m.shape[1]
     m2 = m.transpose()
     seeds = []
     for i in range(0, n_points):
+        # print(i)
         if _eps_neighborhood(point_id, i, eps):
             seeds.append(i)
-    print("End Run  _region_query")
+    # print("End Run  _region_query")
     return seeds
 
 
 def _expand_cluster(m, classifications, point_id, cluster_id, eps, min_points):
-    print("Run _expand_cluster")
+    # print("Run _expand_cluster")
     seeds = _region_query(m, point_id, eps)
     if len(seeds) < min_points:
         classifications[point_id] = NOISE
@@ -54,39 +55,21 @@ def _expand_cluster(m, classifications, point_id, cluster_id, eps, min_points):
                             seeds.append(result_point)
                         classifications[result_point] = cluster_id
             seeds = seeds[1:]
-        print("End Run _expand_cluster")
+        # print("End Run _expand_cluster")
         return True
 
 
 def dbscan(m, eps, min_points):
-    print("Run dbscan")
-
-    """Implementation of Density Based Spatial Clustering of Applications with Noise
-    See https://en.wikipedia.org/wiki/DBSCAN
-    
-    scikit-learn probably has a better implementation
-    
-    Uses Euclidean Distance as the measure
-    
-    Inputs:
-    m - A matrix whose columns are feature vectors
-    eps - Maximum distance two points can be to be regionally related
-    min_points - The minimum number of points to make a cluster
-    
-    Outputs:
-    An array with either a cluster id number or dbscan.NOISE (None) for each
-    column vector in m.
-    """
     cluster_id = 1
     n_points = m.shape[1]
     print(n_points)
     classifications = [UNCLASSIFIED] * n_points
     for point_id in range(0, n_points):
+        print(f"point_id : {point_id}")
         point = m[:, point_id]
         if classifications[point_id] == UNCLASSIFIED:
             if _expand_cluster(m, classifications, point_id, cluster_id, eps, min_points):
                 cluster_id = cluster_id + 1
-    print("End Run dbscan")
     return classifications
 
 
@@ -95,9 +78,11 @@ def main(d, eps, min_points):
     global dists
     print("Run distance")
     dists = distance.cdist(d.transpose(), d.transpose(), 'euclidean')
-    print("end Run distance")
+    print("end distance")
+    print("Run dbscan")
     clusters = dbscan(d, eps, min_points)
-    print("end Run main")
+    print("end dbscan")
+    print("end main")
     return clusters
 
 
