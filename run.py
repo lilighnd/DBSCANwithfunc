d5eb1ff84febd8e4668662fd47becbc810cb9356
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-22 15:10:50 +0430

    First Commit

---
+++ run.py
@@ -0,0 +1,20 @@
+from re import M
+from dbscan import *
+import pandas as pd
+import numpy as np
+import time
+
+
+start_time = time.time()
+path = f'./DataSets/blobsData.csv'
+df = pd.read_csv(path)
+data = df.values.tolist()
+d = np.array(data)
+eps = 0.7
+minpoint = 3
+print(d.shape)
+#c = dbscan(d.transpose(),eps,minpoint)
+c = main(d.transpose() ,eps ,minpoint )
+print(c)
+alltime=time.time() - start_time
+print(alltime)
