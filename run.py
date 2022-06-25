1d764bc36d7b1a921cc360b94f5b66bf686ed0f6
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-25 11:18:09 +0430

    Update : moons,0.07,3

--- run.py
+++ run.py
@@ -3,21 +3,19 @@ from dbscan import *
 import pandas as pd
 import numpy as np
 import time
-from sklearn import cluster, datasets
 
-# blobs = datasets.make_blobs(n_samples=25000, n_features = 2, centers = 3)
-# data=blobs[0]
-start_time = time.time()
-# path = f'./blobs/blobsData1m.csv'
-path = f'/content/drive/MyDrive/Colab Notebooks/blobsData1m.csv'
+
+# path = f'./DataSets/blobsData.csv'
+path = f'/content/drive/MyDrive/Colab Notebooks/moonsData38.csv'
 df = pd.read_csv(path)
 data = df.values.tolist()
 d = np.array(data)
-eps = 1
-minpoint = 5
+eps = 0.07
+minpoint = 3
 print(d.shape)
+start_time = time.time()
 #c = dbscan(d.transpose(),eps,minpoint)
 c = main(d.transpose() ,eps ,minpoint )
-print(c)
 alltime=time.time() - start_time
 print(alltime)
+print(c)
