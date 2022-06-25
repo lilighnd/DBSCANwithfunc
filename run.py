631948c1ebf26b7cf3785b7b0f6ce1d641e45115
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-25 18:39:07 +0430

    Update : X_test,Y_tset,Add True_Labels

--- run.py
+++ run.py
@@ -3,13 +3,22 @@ from dbscan import *
 import pandas as pd
 import numpy as np
 import time
+from sklearn.model_selection import train_test_split
 
 
 # path = f'./DataSets/blobsData.csv'
 path = f'/content/drive/MyDrive/Colab Notebooks/moonsData38.csv'
 df = pd.read_csv(path)
 data = df.values.tolist()
-d = np.array(data)
+
+path = f'/content/drive/MyDrive/Colab Notebooks/moonsLabels38.csv'
+df = pd.read_csv(path)
+Labels = df.values.tolist()
+
+X_train,_,Y_train,_ = train_test_split(data,Labels,test_size=0.33,random_state=42)
+print(X_train)
+# d = np.array(data)
+d = np.array(X_train)
 eps = 0.07
 minpoint = 3
 print(d.shape)
