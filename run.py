1a8954b097869baf599133e527b2cb38f72078ea
lilighnd <ghanadzadehleila5@gmail.com>
2022-08-13 17:34:21 +0430

    u

--- run.py
+++ run.py
@@ -128,14 +128,14 @@ Data=data[0]
 # print(f"Labels : {True_label}")
 # print(f"Data : {Data}")
 
-
+True_label=[]
 path = f'/content/drive/MyDrive/aggregation.csv'
 df = pd.read_csv(path)
 data = df.values.tolist()
 for i in range(len(data)):
     True_label.append(data[i][-1])
     data[i] = data[i][0:2]
-    
+
 True_label = data[1]
 Data=data[0]
 
